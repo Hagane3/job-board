1ceed56798fbdbd7bb9aeba2fd0ca007786e5a75
Konrad <konslo96@gmail.com>
2023-01-19 20:02:17 +0100

    [feat] Add basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import JobsList from "./pages/JobsList";
+import Layout from "./pages/Layout";
+import JobDetails from "./pages/JobDetails";
+
 import "./App.scss";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <JobsList />,
+      },
+      {
+        path: ":jobId",
+        element: <JobDetails />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
