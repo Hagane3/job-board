1f1c0ac864c9857a0ce492cbb8264d9d9fd1f625
Konrad <konslo96@gmail.com>
2023-01-19 21:49:19 +0100

    [feat] Add Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import classes from "./Footer.module.scss";
+
+function Footer() {
+  return (
+    <footer className={classes.footer}>
+      <p>
+        created by <a href="https://github.com/Hagane3">Hagane</a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
